90ab3b32cda1b7859e7c283a37b68ab8ddf793ad
Bartek J <bartekj141@wp.pl>
2021-05-26 17:53:58 +0200

    Initial git

---
+++ src/test/java/com/jakubartlomiej/rentcarrestapi/RentCarRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubartlomiej.rentcarrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
