0bd58cd76bd85b19288e82387df9137f9463fdbb
Bartek J <bartekj141@wp.pl>
2021-06-08 16:52:29 +0200

    added configuration class for swagger

---
+++ src/main/java/com/jakubartlomiej/rentcarrestapi/configuration/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package com.jakubartlomiej.rentcarrestapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
