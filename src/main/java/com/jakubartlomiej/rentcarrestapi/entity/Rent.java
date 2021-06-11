fd3c41ec9acab41a680758b723f435f5b6393f4b
Bartek J <bartekj141@wp.pl>
2021-06-11 21:04:08 +0200

    added rent entity and repository

--- src/main/java/com/jakubartlomiej/rentcarrestapi/entity/Rent.java
+++ src/main/java/com/jakubartlomiej/rentcarrestapi/entity/Rent.java
@@ -1,2 +1,30 @@
-package com.jakubartlomiej.rentcarrestapi.entity;public class Rent {
+package com.jakubartlomiej.rentcarrestapi.entity;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "RENT")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate rentDay;
+    private LocalDate rentEnd;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
 }
