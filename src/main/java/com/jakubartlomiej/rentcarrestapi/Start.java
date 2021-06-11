734a139d867432034c41a5173820ebd65d72c4b6
Bartek J <bartekj141@wp.pl>
2021-06-11 21:05:44 +0200

    added inserting rent to database

--- src/main/java/com/jakubartlomiej/rentcarrestapi/Start.java
+++ src/main/java/com/jakubartlomiej/rentcarrestapi/Start.java
@@ -2,25 +2,30 @@ package com.jakubartlomiej.rentcarrestapi;
 
 import com.jakubartlomiej.rentcarrestapi.entity.Car;
 import com.jakubartlomiej.rentcarrestapi.entity.Client;
+import com.jakubartlomiej.rentcarrestapi.entity.Rent;
 import com.jakubartlomiej.rentcarrestapi.repository.CarRepository;
 import com.jakubartlomiej.rentcarrestapi.repository.ClientRepository;
+import com.jakubartlomiej.rentcarrestapi.repository.RentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Component
 public class Start {
 
     private final CarRepository carRepository;
     private final ClientRepository clientRepository;
+    private final RentRepository rentRepository;
 
     @Autowired
-    public Start(CarRepository carRepository, ClientRepository clientRepository) {
+    public Start(CarRepository carRepository, ClientRepository clientRepository, RentRepository rentRepository) {
         this.carRepository = carRepository;
         this.clientRepository = clientRepository;
+        this.rentRepository = rentRepository;
     }
 
     @EventListener(ApplicationReadyEvent.class)
@@ -35,6 +40,8 @@ public class Start {
         Client client3 = new Client(3L, "Anna", "Borowska", 11445566);
         Client client4 = new Client(4L, "Tomasz", "Noga", 123456789);
 
+        Rent rent = new Rent(1L, LocalDate.of(2021, 6, 11), LocalDate.of(2021, 6, 18),client1,car1 );
+
         carRepository.save(car1);
         carRepository.save(car2);
         carRepository.save(car3);
@@ -44,5 +51,7 @@ public class Start {
         clientRepository.save(client2);
         clientRepository.save(client3);
         clientRepository.save(client4);
+
+        rentRepository.save(rent);
     }
 }
